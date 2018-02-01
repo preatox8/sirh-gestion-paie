69e1265d5f5fc61f40612a8907b76e9e24265a37
Jordan F <ferrejordan8@gmail.com>
2018-02-01 17:39:37 +0100

    TP Spring data JPA

--- src/main/java/dev/paie/repository/AvantageRepository.java
+++ src/main/java/dev/paie/repository/AvantageRepository.java
@@ -1,13 +1,12 @@
 package dev.paie.repository;
 
-import java.math.BigDecimal;
+import java.util.List;
 
 import org.springframework.data.jpa.repository.JpaRepository;
-import org.springframework.data.jpa.repository.Query;
-import org.springframework.data.repository.query.Param;
 
 import dev.paie.entite.Avantage;
 
 public interface AvantageRepository extends JpaRepository<Avantage, Integer> {
 	
+	List<Avantage> findByCode(String code);
 }
